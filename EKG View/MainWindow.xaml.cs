94790836ffe3850393fd831f8cf5515dc150319e
Alexander Oxklint <alex.oxklint@gmail.com>
2020-01-30 12:22:20 +0100

    Implemented record length adjuster, clear trace and invert. Set sample speed to 100 S/s.

--- EKG View/MainWindow.xaml.cs
+++ EKG View/MainWindow.xaml.cs
@@ -22,20 +22,22 @@ namespace EKG_View
 {
     public partial class MainWindow : Window
     {
-        private const int maxDataPoints = 10000;
+        private int maxDataPoints = 250;
         private float t = 0;
         public Timer GraphDataTimer;
         WaveInEvent waveIn = new WaveInEvent();
         private Tuple<LinkedList<float>, LinkedList<float>> data = 
             new Tuple<LinkedList<float>, LinkedList<float>>(new LinkedList<float>(), new LinkedList<float>());
         private bool recording = false;
+        private bool invert = false;
 
         public MainWindow()
         {
             InitializeComponent();
 
-            GraphDataTimer = new Timer(50);
+            GraphDataTimer = new Timer(32);
             GraphDataTimer.Start();
+            waveIn.WaveFormat = new WaveFormat(50, 16, 1);
             GraphDataTimer.Elapsed += GraphDataTimer_Elapsed;
             waveIn.DataAvailable += OnDataAvailable;
 
@@ -45,30 +47,41 @@ namespace EKG_View
 
         private void OnDataAvailable(object sender, WaveInEventArgs args)
         {
-            lock(data)
-            for(int i = 0; i < args.BytesRecorded; i += 2)
+            lock (data)
             {
-                short sample = (short)((args.Buffer[i + 1] << 8) |
-                                args.Buffer[i + 0]);
+                for (int i = 0; i < args.BytesRecorded; i += 2)
+                {
+                    short sample = (short)((args.Buffer[i + 1] << 8) |
+                                    args.Buffer[i + 0]);
 
-                float sample32 = sample / 32786f;
-                data.Item1.AddLast(t); t += 1.0f / (float)waveIn.WaveFormat.AverageBytesPerSecond;
-                data.Item2.AddLast(sample);
-            }
+                    float sample32 = sample / 32786f;
 
-            while(data.Item1.Count > maxDataPoints)
-            {
-                data.Item1.RemoveFirst();
-                data.Item2.RemoveFirst();
+                    if (invert)
+                    {
+                        sample32 = -sample32;
+                    }
+
+                    data.Item1.AddLast(t); t += 1.0f / (float)waveIn.WaveFormat.AverageBytesPerSecond;
+                    data.Item2.AddLast(sample);
+                }
+
+                while (data.Item1.Count > maxDataPoints)
+                {
+                    data.Item1.RemoveFirst();
+                    data.Item2.RemoveFirst();
+                }
             }
         }
         private void GraphDataTimer_Elapsed(object sender, ElapsedEventArgs e)
         {
-            Dispatcher.Invoke(() =>
+            
+            lock (data)
             {
-                lock (data)
-                linegraph.Plot(data.Item1, data.Item2);
-            });
+                Dispatcher.Invoke(() =>
+                {
+                    linegraph.Plot(data.Item1, data.Item2);
+                });
+            }
         }
         private void btn_stop_Click(object sender, RoutedEventArgs e)
         {
@@ -80,22 +93,35 @@ namespace EKG_View
             if (!recording)
             {
                 waveIn.StartRecording();
+                linegraph.IsAutoFitEnabled = true;
                 recording = true;
             }
         }
         private void btn_clr_Click(object sender, RoutedEventArgs e)
         {
-            lock(data)
-            while(data.Item1.Count != 0)
+            lock (data)
             {
-                data.Item1.RemoveFirst();
-                data.Item2.RemoveFirst();
+                while (data.Item1.Count != 0)
+                {
+                    data.Item1.RemoveFirst();
+                    data.Item2.RemoveFirst();
+                }
             }
         }
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
             GraphDataTimer.Stop();
             waveIn.StopRecording();
+
+            System.Threading.Thread.Sleep(100);
+        }
+        private void cb_Invert_Checked(object sender, RoutedEventArgs e)
+        {
+            invert = (bool)cb_Invert.IsChecked;
+        }
+        private void slider_samples_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            maxDataPoints = (int)slider_samples.Value;
         }
     }
 }
